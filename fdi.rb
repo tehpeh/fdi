c642311b5e584a9837f7e2ee1ea1e499044ed8b7
Tim Preston <tim@tp.id.au>
2011-08-06 17:28:38 +1000

    Added base attributes to Obs and FDI calculation.

--- fdi.rb
+++ fdi.rb
@@ -2,18 +2,40 @@ require 'bundler/setup'
 require 'sinatra'
 require 'datamapper'
 
+# models
 class Obs
   include DataMapper::Resource
-  property :id,         Serial
-  property :time,       DateTime, :required => true
+  property :id,                 Serial,   :index => true
+  property :time,               DateTime, :required => true
+  property :temperature,        Float,    :required => true
+  property :drought_factor,     Integer,  :required => true
+  property :wind_speed,         Float,    :required => true
+  property :relative_humidity,  Float,    :required => true
+  property :fdi,                Integer,  :index => true, :writer => :protected
   timestamps :created_at, :updated_at
+
+  before :save, :calculate_fdi
+
+  def self.recalculate_fdis
+    Obs.all.each do |o|
+      o.calculate_fdi
+      o.save
+    end
+  end
+
+  def calculate_fdi
+    self.fdi = ( 7.0 / drought_factor ) *
+      ( wind_speed / ( 10 - ( (temperature - relative_humidity) / 4.0))).round.to_i
+  end
 end
 DataMapper.finalize
 
+# lib
 def process
   ENV['_'].split('/').last
 end
 
+# sinatra
 configure :development do
   require 'sinatra/reloader'
   DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/fdi_development.sqlite3")
@@ -23,10 +45,16 @@ configure :development do
   end
 end
 
+# controllers
 configure :test do
   DataMapper.setup(:default, 'sqlite::memory:')
 end
 
 get '/' do
   status 200
+  out = ""
+  Obs.all.each do |o|
+    out += "#{o.to_json}<br>"
+  end
+  "Obs:<br>#{out}"
 end
--- spec/fdi_spec.rb
+++ spec/fdi_spec.rb
@@ -10,26 +10,41 @@ describe 'fdi' do
 end
 
 describe Obs do
-  let(:valid_attributes) { { :time => DateTime.now } }
+  let(:valid_attributes) { {
+    :time => DateTime.now,
+    :temperature => 30.0,
+    :drought_factor => 7,
+    :wind_speed => 60.0,
+    :relative_humidity => 10.0 } }
   let(:obs) { Obs.create(valid_attributes) }
 
   it 'is valid with valid attributes' do
     obs.should be_valid
   end
 
-  it 'has attribute created_at' do
+  it 'has timestamps' do
     obs.created_at.should be_within(0.1).of(DateTime.now)
-  end
-
-  it 'has attribute updated_at' do
     obs.updated_at.should be_within(0.1).of(DateTime.now)
-  end
-
-  it 'has attribute time' do
-    obs.time.should == valid_attributes[:time]
   end
 
   it 'is not valid without required attributes' do
     Obs.new.should_not be_valid
+  end
+
+  it 'does not allow the fdi to be written' do
+    expect { obs.fdi = 100 }.to raise_error
+  end
+
+  it 'calculates the FDI on save' do
+    obs.fdi.should == 12
+  end
+
+  describe '.recalculate_fdis' do
+    it 'recalculates the fdi for all records' do
+      Obs.stub(:all).and_return([obs])
+      obs.should_receive :calculate_fdi
+      obs.should_receive :save
+      Obs.recalculate_fdis
+    end
   end
 end
